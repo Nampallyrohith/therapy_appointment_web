da0f12c282d4902c4ee8d40bf6713904003bd100
Nampally Rohith <rohithnampelly57@gmail.com>
2025-01-04 17:50:31 +0530

    footer component loc changed to shared

--- src/Handler/ProtectedRoute.tsx
+++ src/Handler/ProtectedRoute.tsx
@@ -1,4 +1,4 @@
-import Footer from "@/components/Footer";
+import Footer from "@/shared/Footer";
 import Header from "@/shared/Header";
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
