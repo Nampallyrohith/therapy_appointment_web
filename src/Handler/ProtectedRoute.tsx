a1d5fc6a5e4500fe15c2fe8bcaf643b956b7ead8
ManikantaPinabakala <manikantapinabakala@gmail.com>
2025-02-20 13:29:49 +0530

    handling login sessions

--- src/Handler/ProtectedRoute.tsx
+++ src/Handler/ProtectedRoute.tsx
@@ -2,27 +2,71 @@ import { useAppointmentContext } from "@/context/AppointmentContext";
 import Footer from "@/shared/Footer";
 import Header from "@/shared/Header";
 import Loader from "@/shared/Loader";
-import { Navigate, Outlet, useLocation } from "react-router-dom"; 
+import { Navigate, Outlet, useLocation, useNavigate } from "react-router-dom";
+import Modal from "react-modal";
+import { Button } from "@chakra-ui/react";
+
+Modal.setAppElement("#root");
+
 const ProtectedRoute = () => {
-  const { isAuthToken, isLoading } = useAppointmentContext();
-  const location = useLocation(); // Get current location
+  const { isAuthToken, isLoading, isSessionExpired, handleUserSignOut } =
+    useAppointmentContext();
+  const location = useLocation();
+  const navigate = useNavigate();
 
   if (isLoading) {
-      return <Loader />;
+    return <Loader />;
   }
 
   if (!isAuthToken) {
-      // Store the intended URL before redirecting
-      sessionStorage.setItem('intendedRoute', location.pathname); // or localStorage
-      return <Navigate to="/login" />;
+    sessionStorage.setItem("intendedRoute", location.pathname);
+    return <Navigate to="/login" />;
+  }
+
+  if (isSessionExpired) {
+    return (
+      <Modal
+        isOpen={true}
+        shouldCloseOnOverlayClick={false}
+        shouldCloseOnEsc={false}
+        style={{
+          overlay: { backgroundColor: "rgba(0, 0, 0, 0.75)" },
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            marginRight: "-50%",
+            transform: "translate(-50%, -50%)",
+            padding: "20px 40px",
+            textAlign: "center",
+            borderRadius: "10px",
+          },
+        }}
+      >
+        <h2 className="text-xl font-semibold text-red-600">Session Expired</h2>
+        <p className="mt-4 mb-2">
+          Your session has expired. Please log in again.
+        </p>
+        <Button
+          onClick={() => {
+            handleUserSignOut();
+            navigate("/login");
+          }}
+          className="mt-4 bg-orange-primary-1 p-3 text-white"
+        >
+          Return to Login
+        </Button>
+      </Modal>
+    );
   }
 
   return (
-      <>
-          <Header />
-          <Outlet />
-          <Footer />
-      </>
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
   );
 };
 
